f38617f1e2e100d1aaab59f740e51ca1933c5987
Fabio Mannis <fabio.mannis@outlook.com>
2024-12-15 11:25:14 +0100

    1st commit

---
+++ darthHelperLibs/CollectionHelper/CollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace darthHelperLibs.CollectionHelper
+{
+    public static class CollectionExtension
+    {
+        public static bool IsEmptyOrNull<T>(this IEnumerable<T> collection)
+        {
+            return collection?.Any() != true;
+        }
+    }
+}
---
+++ darthHelperLibs/StringHelper/StringExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace darthHelperLibs.StringHelper
+{
+    public static class StringExtension
+    {
+        public static string BuildString(this IEnumerable<string>? values)
+        {
+            if (values == null) return string.Empty;
+            var valueList = values.ToList();
+            if(valueList.Any()) return string.Empty;
+
+            var estimatedCapacity = valueList.Sum(v => v.Length);
+            var strBuilder = new StringBuilder(estimatedCapacity);
+
+            foreach (var value in valueList)
+            {
+                strBuilder.Append(value);
+            }
+
+            return strBuilder.ToString();
+        }
+    }
+}
